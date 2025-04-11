677bddf5084252880d36d40c7e226dc06016a97a
Muhamad Teguh Prayoga <102048130+tprayoga@users.noreply.github.com>
2025-04-11 14:58:38 +0700

    7

--- src/App.js
+++ src/App.js
@@ -8,63 +8,43 @@ const App = () => {
   };
 
   return (
-<div>
-  <table
-    style={{
-      fontSize: '9px',
-      fontFamily: 'monospace',
-      width: '100%',
-      borderCollapse: 'collapse',
-      fontWeight: 'bold',
-    }}
-  >
-    <thead>
-      <tr>
-        <th style={{ border: '1px solid #000', padding: '2px' }}>Item</th>
-        <th style={{ border: '1px solid #000', padding: '2px' }}>Qty</th>
-        <th style={{ border: '1px solid #000', padding: '2px' }}>Harga</th>
-        <th style={{ border: '1px solid #000', padding: '2px' }}>Subtotal</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>Nasi Goreng</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>1</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>15.000</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>15.000</td>
-      </tr>
-      <tr>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>Teh Manis</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>2</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>5.000</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>10.000</td>
-      </tr>
-      <tr>
-        <td colSpan="3" style={{ border: '1px solid #000', padding: '2px' }}>Total</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>25.000</td>
-      </tr>
-      <tr>
-        <td colSpan="3" style={{ border: '1px solid #000', padding: '2px' }}>Bayar</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>30.000</td>
-      </tr>
-      <tr>
-        <td colSpan="3" style={{ border: '1px solid #000', padding: '2px' }}>Kembali</td>
-        <td style={{ border: '1px solid #000', padding: '2px' }}>5.000</td>
-      </tr>
-    </tbody>
-  </table>
+    <div
+  style={{
+    fontFamily: 'monospace',
+    fontSize: '8px',
+    fontWeight: 'bold',
+    whiteSpace: 'pre',
+    lineHeight: '1.4',
+  }}
+>
+  <div style={{ textAlign: 'center' }}>
+    TOKO MAKMUR JAYA
+    <br />
+    Jl. Kebon Jeruk No.12
+    <br />
+    --------------------------------
+  </div>
 
-  <div
-    style={{
-      fontSize: '9px',
-      fontFamily: 'monospace',
-      fontWeight: 'bold',
-      marginTop: '10px',
-      textAlign: 'center',
-    }}
-  >
-    <div>Terima Kasih Telah Berbelanja!</div>
-    <div>11-04-2025 14:32:11</div>
+  <div>
+    Nasi Goreng           x1   15.000
+    <br />
+    Teh Manis             x2   10.000
+    <br />
+    --------------------------------
+    <br />
+    Total:                     25.000
+    <br />
+    Bayar:                     30.000
+    <br />
+    Kembali:                   5.000
+    <br />
+    --------------------------------
+  </div>
+
+  <div style={{ textAlign: 'center' }}>
+    Terima Kasih!
+    <br />
+    11-04-2025 14:32:11
   </div>
 
   <button
@@ -75,12 +55,12 @@ const App = () => {
       fontSize: '12px',
       cursor: 'pointer',
       fontWeight: 'bold',
+      display: 'block',
     }}
   >
     Print
   </button>
 </div>
-
 
   );
 };

5249d3057549782466c6895990b62579fc5da15d
Muhamad Teguh Prayoga <102048130+tprayoga@users.noreply.github.com>
2025-04-11 14:48:53 +0700

    4

--- src/App.js
+++ src/App.js
@@ -8,53 +8,60 @@ const App = () => {
   };
 
   return (
-    <div>
-      <table className="print-receipt">
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-          </tr>
-          <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-          </tr>
-        </tbody>
-      </table>
-      <h1>Test Print Nota</h1>
-      <div  className="print-receipt" >
-        <div>       TOKO MAKMUR JAYA</div>
-        <div>     Jl. Kebon Jeruk No.12</div>
-        <div>--------------------------------</div>
-        <div>1x Nasi Goreng         15.000</div>
-        <div>2x Teh Manis           10.000</div>
-        <div>--------------------------------</div>
-        <div>Total:                25.000</div>
-        <div>Bayar:                30.000</div>
-        <div>Kembali:               5.000</div>
-        <div>--------------------------------</div>
-        <div>     Terima Kasih!</div>
-        <div>  11-04-2025 14:32:11</div>
-      </div>
+<div>
+  <table
+    style={{
+      fontSize: '9px',
+      fontFamily: 'monospace',
+      width: '100%',
+      borderCollapse: 'collapse',
+    }}
+  >
+    <thead>
+      <tr>
+        <th style={{ border: '1px solid #000', padding: '2px', textAlign: 'left' }}>
+          Company
+        </th>
+        <th style={{ border: '1px solid #000', padding: '2px', textAlign: 'left' }}>
+          Contact
+        </th>
+        <th style={{ border: '1px solid #000', padding: '2px', textAlign: 'left' }}>
+          Country
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Alfreds Futterkiste</td>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Maria Anders</td>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Germany</td>
+      </tr>
+      <tr>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Centro comercial Moctezuma</td>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Francisco Chang</td>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Mexico</td>
+      </tr>
+      <tr>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Ernst Handel</td>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Roland Mendel</td>
+        <td style={{ border: '1px solid #000', padding: '2px' }}>Austria</td>
+      </tr>
+    </tbody>
+  </table>
 
-      <button className="hide-on-print" onClick={printReceipt}>
-        Print
-      </button>
-    </div>
+  <button
+    onClick={printReceipt}
+    style={{
+      marginTop: '10px',
+      padding: '6px 12px',
+      fontSize: '12px',
+      cursor: 'pointer',
+    }}
+  >
+    Print
+  </button>
+</div>
+
   );
 };
 
